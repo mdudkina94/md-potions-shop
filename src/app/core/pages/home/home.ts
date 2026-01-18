6d5fa14055f2ce94e180865b562a2b481c093893
Margarita Dudkina <m.dudkina94@gmail.com>
2026-01-18 11:46:43 +0300

    update topbar

--- src/app/core/pages/home/home.ts
+++ src/app/core/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { AppFooter } from '@/layout/component/app.footer';
+import { AppSidebar } from '@/layout/component/app.sidebar';
+import { AppTopbar } from '@/layout/component/app.topbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.scss'
+    selector: 'app-home',
+    templateUrl: './home.html',
+    styleUrl: './home.scss',
+    imports: [AppFooter, AppSidebar, AppTopbar, RouterOutlet],
+    standalone: true
 })
-export class Home {
-
-}
+export class Home {}
