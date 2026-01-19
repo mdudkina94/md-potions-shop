76c2d6538ca109dc660a24daaad0b6e7560d39d1
Margarita Dudkina <m.dudkina94@gmail.com>
2026-01-20 01:09:47 +0300

    base ingredients list

--- src/app/ingredients/ingredients-list/ingredients-list.ts
+++ src/app/ingredients/ingredients-list/ingredients-list.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Ingredient } from '@/ingredients/ingredients.types';
+import { INGREDIENTS_LIST } from '@/ingredients/ingredients.data';
 
 @Component({
   selector: 'app-ingredients-list',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './ingredients-list.html',
-  styleUrl: './ingredients-list.scss'
+  styleUrl: './ingredients-list.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IngredientsList {
-
+  readonly ingredients: Ingredient[] = INGREDIENTS_LIST;
 }
--- src/app/ingredients/ingredients.data.ts
+++ src/app/ingredients/ingredients.data.ts
@@ -1,6 +1,6 @@
 import { Ingredient } from './ingredients.types';
 
-export const IngredientsList: Array<Ingredient> = [
+export const INGREDIENTS_LIST: Array<Ingredient> = [
   {
     id: 1,
     name: 'Сок Солнечного Цветка',
@@ -11,10 +11,169 @@ export const IngredientsList: Array<Ingredient> = [
     name: 'Пыльца Феи',
     unitPrice: 200
   },
-  { id: 3, name: 'Слеза Единорога', unitPrice: 1000 },
+  {
+    id: 3,
+    name: 'Слеза Единорога',
+    unitPrice: 1000
+  },
   {
     id: 4,
     name: 'Чистая Родниковая Вода',
     unitPrice: 10
+  },
+  {
+    id: 5,
+    name: 'Корень Тенецвета',
+    unitPrice: 120
+  },
+  {
+    id: 6,
+    name: 'Пыль Призрака',
+    unitPrice: 350
+  },
+  {
+    id: 7,
+    name: 'Чешуя Хамелеона',
+    unitPrice: 80
+  },
+  {
+    id: 8,
+    name: 'Коготь Грифона',
+    unitPrice: 400
+  },
+  {
+    id: 9,
+    name: 'Сердце Гиганта (сушеное)',
+    unitPrice: 1500
+  },
+  {
+    id: 10,
+    name: 'Руда Железного Кулака',
+    unitPrice: 30
+  },
+  {
+    id: 11,
+    name: 'Эссенция Берсерка',
+    unitPrice: 250
+  },
+  {
+    id: 12,
+    name: 'Перо Грифона',
+    unitPrice: 90
+  },
+  {
+    id: 13,
+    name: 'Эссенция Облаков',
+    unitPrice: 180
+  },
+  {
+    id: 14,
+    name: 'Пыль Зефира',
+    unitPrice: 100
+  },
+  {
+    id: 15,
+    name: 'Четырехлистный Клевер',
+    unitPrice: 75
+  },
+  {
+    id: 16,
+    name: 'Лапка Кролика (заколдованная)',
+    unitPrice: 300
+  },
+  {
+    id: 17,
+    name: 'Золотая Пыль Лепрекона',
+    unitPrice: 2000
+  },
+  {
+    id: 18,
+    name: 'Лепесток Веритаса',
+    unitPrice: 150
+  },
+  {
+    id: 19,
+    name: 'Слеза Сирены',
+    unitPrice: 700
+  },
+  {
+    id: 20,
+    name: 'Кристалл Чистоты',
+    unitPrice: 450
+  },
+  {
+    id: 21,
+    name: 'Шепот Ветра (сушеный)',
+    unitPrice: 60
+  },
+  {
+    id: 22,
+    name: 'Ягода Ночницы',
+    unitPrice: 40
+  },
+  {
+    id: 23,
+    name: 'Нить Ловца Снов',
+    unitPrice: 110
+  },
+  {
+    id: 24,
+    name: 'Лунная Роса',
+    unitPrice: 90
+  },
+  {
+    id: 25,
+    name: 'Чешуя Саламандры',
+    unitPrice: 130
+  },
+  {
+    id: 26,
+    name: 'Вулканический Пепел',
+    unitPrice: 25
+  },
+  {
+    id: 27,
+    name: 'Пылающий Камень',
+    unitPrice: 380
+  },
+  {
+    id: 28,
+    name: 'Кровь Огненной Виверны',
+    unitPrice: 900
+  },
+  {
+    id: 29,
+    name: 'Водоросли Гиллевида',
+    unitPrice: 60
+  },
+  {
+    id: 30,
+    name: 'Чешуя Тритона',
+    unitPrice: 170
+  },
+  {
+    id: 31,
+    name: 'Жемчужина Глубин',
+    unitPrice: 500
+  },
+  {
+    id: 32,
+    name: 'Усы Теневого Кота',
+    unitPrice: 100
+  },
+  {
+    id: 33,
+    name: 'Молоко Луны',
+    unitPrice: 220
+  },
+  {
+    id: 34,
+    name: 'Мох Перевертыша',
+    unitPrice: 140
+  },
+  {
+    id: 35,
+    name: 'Отпечаток Лапы Крадущегося',
+    unitPrice: 190
   }
 ];
